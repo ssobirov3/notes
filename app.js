ffcc44373dab248cbdb45a3cdcf4b1fc75454543
Sobirbek Sobirov <ssobirov3@students.wiut.uz>
2021-03-12 17:57:59 +0500

    add some new pages and some styling

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express=require('express');
 const app=express();
 
+const notes=[
+	'Some notes',
+	'Some notes-2',
+	'Some notes-3'
+];
+
+
 app.use('/static',express.static('public'))
 
 app.set('view engine', 'pug');
@@ -9,6 +16,24 @@ app.get('/',(req,res) =>{
 	//to do -- Handler for Root URL 
 	res.render('home');
 })
+
+app.get('/create',(req,res) =>{
+	//to do -- Handler for Root URL 
+	res.render('create');
+})
+
+
+app.get('/notes',(req,res) =>{
+	//to do -- Handler for Root URL 
+	res.render('notes', { n: notes });
+})
+
+
+app.get('/notes/1',(req,res) =>{
+	//to do -- Handler for Root URL 
+	res.render('detail');
+})
+
 
 app.listen(8000, (err) =>{
 	if(err) throw err
